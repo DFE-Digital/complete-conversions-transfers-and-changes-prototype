501a6f2ab49e8206cc26c0535ca0f5d3bca1b5f8
Rich-Cooley <91479782+Rich-Cooley@users.noreply.github.com>
2022-06-22 10:56:33 +0100

    Set up versions in prototype
    - view and routes folders created
    - main routes expects version number of routes files to allow different
      versions to work uniquely
    - created basic start page so that there is a thing to link to

--- app/routes.js
+++ app/routes.js
@@ -2,5 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
+require('./routes/0-1/routes.js')(router);
+
 
 module.exports = router

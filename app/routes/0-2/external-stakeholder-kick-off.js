1341b3e27c996470743c9e12111f7fb197b32f13
titlescreen <richardcooley@gmail.com>
2022-07-08 09:57:56 +0100

    Add in check answers page and link back to task list

--- app/routes/0-2/external-stakeholder-kick-off.js
+++ app/routes/0-2/external-stakeholder-kick-off.js
@@ -19,4 +19,22 @@ module.exports = function (router) {
     res.redirect('had-kick-off')
   })
 
+
+  router.get('/' + version + '/external-stakeholder-kick-off/had-kick-off', function (req, res) {
+    res.render(version + '/external-stakeholder-kick-off/had-kick-off', {})
+  })
+
+  router.post('/' + version + '/external-stakeholder-kick-off/had-kick-off', function (req, res) {
+    res.redirect('check-answers')
+  })
+
+
+  router.get('/' + version + '/external-stakeholder-kick-off/check-answers', function (req, res) {
+    res.render(version + '/external-stakeholder-kick-off/check-answers', {})
+  })
+
+  router.post('/' + version + '/external-stakeholder-kick-off/check-answers', function (req, res) {
+    res.redirect('../project-task-list')
+  })
+
 }

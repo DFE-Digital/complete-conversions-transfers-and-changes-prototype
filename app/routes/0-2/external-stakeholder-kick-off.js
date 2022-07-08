280def2c5bc676f92aa2840a8e87c6a249aac5f1
titlescreen <richardcooley@gmail.com>
2022-07-08 09:30:21 +0100

    Add in the had kick off call question and guidance

--- app/routes/0-2/external-stakeholder-kick-off.js
+++ app/routes/0-2/external-stakeholder-kick-off.js
@@ -10,4 +10,13 @@ module.exports = function (router) {
     res.redirect('kick-off-date')
   })
 
+
+  router.get('/' + version + '/external-stakeholder-kick-off/kick-off-date', function (req, res) {
+    res.render(version + '/external-stakeholder-kick-off/kick-off-date', {})
+  })
+
+  router.post('/' + version + '/external-stakeholder-kick-off/kick-off-date', function (req, res) {
+    res.redirect('had-kick-off')
+  })
+
 }

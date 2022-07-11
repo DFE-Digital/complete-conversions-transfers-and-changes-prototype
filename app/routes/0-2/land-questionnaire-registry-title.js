0e2da2b28c44d5deca4af3444f87b43d97e93125
titlescreen <richardcooley@gmail.com>
2022-07-11 11:23:54 +0100

    Add clear land questionnaire page

--- app/routes/0-2/land-questionnaire-registry-title.js
+++ app/routes/0-2/land-questionnaire-registry-title.js
@@ -7,8 +7,18 @@ module.exports = function (router) {
   })
 
   router.post('/' + version + '/land-questionnaire-registry-title/check-land-questionnaire', function (req, res) {
+    res.redirect('clear-land-questionnaire')
+  })
+
+
+  router.get('/' + version + '/land-questionnaire-registry-title/clear-land-questionnaire', function (req, res) {
+    res.render(version + '/land-questionnaire-registry-title/clear-land-questionnaire', {})
+  })
+
+  router.post('/' + version + '/land-questionnaire-registry-title/clear-land-questionnaire', function (req, res) {
     res.redirect('check-answers')
   })
+
 
   router.get('/' + version + '/land-questionnaire-registry-title/check-answers', function (req, res) {
     res.render(version + '/land-questionnaire-registry-title/check-answers', {})

d23b68163c1e76983be78d04818dc3650d8c21cf
titlescreen <richardcooley@gmail.com>
2022-07-12 10:48:23 +0100

    Make supplementary funding agreement page

--- app/routes/0-2/additional-documents.js
+++ app/routes/0-2/additional-documents.js
@@ -7,8 +7,18 @@ module.exports = function (router) {
   })
 
   router.post('/' + version + '/additional-documents/check-articles-of-association', function (req, res) {
+    res.redirect('check-supplementary-funding-agreement')
+  })
+
+
+  router.get('/' + version + '/additional-documents/check-supplementary-funding-agreement', function (req, res) {
+    res.render(version + '/additional-documents/check-supplementary-funding-agreement', {})
+  })
+
+  router.post('/' + version + '/additional-documents/check-supplementary-funding-agreement', function (req, res) {
     res.redirect('check-answers')
   })
+
 
   router.get('/' + version + '/additional-documents/check-answers', function (req, res) {
     res.render(version + '/additional-documents/check-answers', {})

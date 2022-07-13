1fc44bcf79487277f35d3488899acc58096b65a0
titlescreen <richardcooley@gmail.com>
2022-07-13 15:29:36 +0100

    Remove all sharepoint URL questions
    As there is a single sharepoint URL we do not need to ask for the path
    to each document.
    
    Also tidy up check answers page to not include these questions

--- app/routes/0-2/project-kick-off.js
+++ app/routes/0-2/project-kick-off.js
@@ -2,22 +2,7 @@ module.exports = function (router) {
 
   var version = "0-2";
 
-  router.get('/' + version + '/project-kick-off/save-proforma', function (req, res) {
-    res.render(version + '/project-kick-off/save-proforma', {})
-  })
 
-  router.post('/' + version + '/project-kick-off/save-proforma', function (req, res) {
-    res.redirect('proforma-url')
-  })
-
-
-  router.get('/' + version + '/project-kick-off/proforma-url', function (req, res) {
-    res.render(version + '/project-kick-off/proforma-url', {})
-  })
-
-  router.post('/' + version + '/project-kick-off/proforma-url', function (req, res) {
-    res.redirect('check-trust-model-docs')
-  })
 
 
   router.get('/' + version + '/project-kick-off/check-trust-model-docs', function (req, res) {
@@ -25,15 +10,6 @@ module.exports = function (router) {
   })
 
   router.post('/' + version + '/project-kick-off/check-trust-model-docs', function (req, res) {
-    res.redirect('trust-model-docs-url')
-  })
-
-
-  router.get('/' + version + '/project-kick-off/trust-model-docs-url', function (req, res) {
-    res.render(version + '/project-kick-off/trust-model-docs-url', {})
-  })
-
-  router.post('/' + version + '/project-kick-off/trust-model-docs-url', function (req, res) {
     res.redirect('check-academy-order')
   })
 
@@ -43,15 +19,6 @@ module.exports = function (router) {
   })
 
   router.post('/' + version + '/project-kick-off/check-academy-order', function (req, res) {
-    res.redirect('academy-order-url')
-  })
-
-
-  router.get('/' + version + '/project-kick-off/academy-order-url', function (req, res) {
-    res.render(version + '/project-kick-off/academy-order-url', {})
-  })
-
-  router.post('/' + version + '/project-kick-off/academy-order-url', function (req, res) {
     res.redirect('check-application-to-convert')
   })
 
@@ -61,15 +28,6 @@ module.exports = function (router) {
   })
 
   router.post('/' + version + '/project-kick-off/check-application-to-convert', function (req, res) {
-    res.redirect('application-to-convert-url')
-  })
-
-
-  router.get('/' + version + '/project-kick-off/application-to-convert-url', function (req, res) {
-    res.render(version + '/project-kick-off/application-to-convert-url', {})
-  })
-
-  router.post('/' + version + '/project-kick-off/application-to-convert-url', function (req, res) {
     res.redirect('identify-things-to-discuss')
   })
 

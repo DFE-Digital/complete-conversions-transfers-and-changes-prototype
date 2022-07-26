0ae9bfaa8876ceae1ebf443d7f410fe9d44f39a3
titlescreen <richardcooley@gmail.com>
2022-07-26 14:26:38 +0100

    Remove received land questionnaire as its implicit in the clear question

--- app/routes/0-3/land-questionnaire-registry-title.js
+++ app/routes/0-3/land-questionnaire-registry-title.js
@@ -2,15 +2,6 @@ module.exports = function (router) {
 
   var version = "0-3";
 
-  router.get('/' + version + '/land-questionnaire-registry-title/check-land-questionnaire', function (req, res) {
-    res.render(version + '/land-questionnaire-registry-title/check-land-questionnaire', {})
-  })
-
-  router.post('/' + version + '/land-questionnaire-registry-title/check-land-questionnaire', function (req, res) {
-    res.redirect('clear-land-questionnaire')
-  })
-
-
   router.get('/' + version + '/land-questionnaire-registry-title/clear-land-questionnaire', function (req, res) {
     res.render(version + '/land-questionnaire-registry-title/clear-land-questionnaire', {})
   })

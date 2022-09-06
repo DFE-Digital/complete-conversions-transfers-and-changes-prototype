549c67c483f06e3bb821857b0aacb94960881a2a
titlescreen <richardcooley@gmail.com>
2022-09-06 10:47:06 +0100

    Process conversion grant status tag works

--- app/routes/0-5/process-conversion-grant.js
+++ app/routes/0-5/process-conversion-grant.js
@@ -2,44 +2,25 @@ module.exports = function (router) {
 
   var version = "0-5";
 
-  // router.get('/' + version + '/process-conversion-grant/single-page-questions', function (req, res) {
-  //   res.render(version + '/process-conversion-grant/single-page-questions', {})
-  // })
-
-  // router.post('/' + version + '/process-conversion-grant/single-page-questions', function (req, res) {
-  //   const checkVendorAccount = req.session.data['checkVendorAccount']
-  //   const checkGrantPaymentForm = req.session.data['checkGrantPaymentForm']
-  //   const checkEmailEsfa = req.session.data['checkEmailEsfa']
-
-  //   if (checkVendorAccount == 'No' ||
-  //     checkVendorAccount == null ||
-  //     checkGrantPaymentForm == 'No' ||
-  //     checkGrantPaymentForm == null ||
-  //     checkEmailEsfa == 'No' ||
-  //     checkEmailEsfa == null){
-  //     req.session.data.processConversionGrantStatus = 'inProgress'
-  //   } else {
-  //     req.session.data.processConversionGrantStatus = 'complete'
-  //   }
-  //   res.redirect('../project-task-list')
-  // })
-
-  router.get('/' + version + '/checklist-test/single-page-questions', function (req, res) {
-    res.render(version + '/checklist-test/single-page-questions', {})
+  router.get('/' + version + '/process-conversion-grant/single-page-questions', function (req, res) {
+    res.render(version + '/process-conversion-grant/single-page-questions', {})
   })
 
-  router.post('/' + version + '/checklist-test/single-page-questions', function (req, res) {
-    const testReceivedDocumentStatus = req.session.data['testReceivedDocumentStatus']
-    const testReceivedDocument = req.session.data['testReceivedDocument']
+  router.post('/' + version + '/process-conversion-grant/single-page-questions', function (req, res) {
+    const checkVendorAccount = req.session.data['checkVendorAccount']
+    const checkGrantPaymentForm = req.session.data['checkGrantPaymentForm']
+    const checkEmailEsfa = req.session.data['checkEmailEsfa']
 
-    if (testReceivedDocument.includes('Cleared document')){
-      req.session.data.testReceivedDocumentStatus = 'complete'
-    }else if (testReceivedDocument.includes('Received document') || testReceivedDocument.includes('Sent to policy team')){
-      req.session.data.testReceivedDocumentStatus = 'inProgress'
-    }else {
-      req.session.data.testReceivedDocumentStatus = 'notStarted'
+    if (checkVendorAccount == 'No' ||
+      checkVendorAccount == null ||
+      checkGrantPaymentForm == 'No' ||
+      checkGrantPaymentForm == null ||
+      checkEmailEsfa == 'No' ||
+      checkEmailEsfa == null){
+      req.session.data.processConversionGrantStatus = 'inProgress'
+    } else {
+      req.session.data.processConversionGrantStatus = 'complete'
     }
-
     res.redirect('../project-task-list')
   })
 
--- app/routes/0-6/process-conversion-grant.js
+++ app/routes/0-6/process-conversion-grant.js
@@ -7,20 +7,24 @@ module.exports = function (router) {
   })
 
   router.post('/' + version + '/process-conversion-grant/single-page-questions', function (req, res) {
-    const checkVendorAccount = req.session.data['checkVendorAccount']
-    const checkGrantPaymentForm = req.session.data['checkGrantPaymentForm']
-    const checkEmailEsfa = req.session.data['checkEmailEsfa']
+    const processConversionGrant = req.session.data['processConversionGrant']
 
-    if (checkVendorAccount == 'No' ||
-      checkVendorAccount == null ||
-      checkGrantPaymentForm == 'No' ||
-      checkGrantPaymentForm == null ||
-      checkEmailEsfa == 'No' ||
-      checkEmailEsfa == null){
-      req.session.data.processConversionGrantStatus = 'inProgress'
-    } else {
+    if (processConversionGrant.includes('checkVendorAccount') &
+      processConversionGrant.includes('checkSchoolEligibility') &
+      processConversionGrant.includes('checkGrantForm') &
+      processConversionGrant.includes('sendToPaymentsTeam') &
+      processConversionGrant.includes('giveSchoolPaymentDate') &
+      processConversionGrant.includes('checkSchoolGotGrant') &
+      processConversionGrant.includes('empty')){
       req.session.data.processConversionGrantStatus = 'complete'
+    }else if (processConversionGrant == ('empty')){
+      req.session.data.processConversionGrantStatus = 'notStarted'
+    }else {
+      req.session.data.processConversionGrantStatus = 'inProgress'
     }
+
+    // Need to make a checkbox for empty that I can look against
+
     res.redirect('../project-task-list')
   })
 

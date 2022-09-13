a283f4734a6cf54987e80f0c38a1cd54c9e5144b
titlescreen <richardcooley@gmail.com>
2022-09-13 15:33:23 +0100

    Make readiness for opening page

--- app/routes.js
+++ app/routes.js
@@ -78,6 +78,7 @@ const router = express.Router()
   require('./routes/0-7/land-registry-title-plans.js')(router);
   require('./routes/0-7/master-funding-agreement.js')(router);
   require('./routes/0-7/process-conversion-grant.js')(router);
+  require('./routes/0-7/readiness-for-opening.js')(router);
   require('./routes/0-7/regional-delivery-officer-handover.js')(router);
   require('./routes/0-7/single-worksheet.js')(router);
   require('./routes/0-7/supplementary-funding-agreement.js')(router);

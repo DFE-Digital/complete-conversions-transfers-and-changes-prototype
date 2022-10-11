5d581bbac6235d0dfe32bd944549eb319bfe17fb
titlescreen <richardcooley@gmail.com>
2022-10-11 15:59:52 +0100

    Add in bunch changes
    - added in sub nav to toggle active/close projects
    - added project list with current school closed
    - edited the content on the project closed page

--- app/routes/0-9/routes.js
+++ app/routes/0-9/routes.js
@@ -39,7 +39,7 @@ module.exports = function (router) {
   })
 
   router.post('/' + version + '/project-closed', function (req, res) {
-    res.redirect('project-list-after-school-closed')
+    res.redirect('project-list-2')
   })
 
 }

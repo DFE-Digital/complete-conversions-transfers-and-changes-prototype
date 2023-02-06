7053d68d9c96e776d44ae97b2c527f612b7c3a98
titlescreen <richardcooley@gmail.com>
2023-02-06 15:55:54 +0000

    Increment to sprint 16
    And track work on index

---
+++ app/routes/0-16/baseline-checks.js
@@ -0,0 +1,13 @@
+module.exports = function (router) {
+
+  var version = "0-16";
+
+  router.get('/' + version + '/baseline-checks/single-page-questions', function (req, res) {
+    res.render(version + '/baseline-checks/single-page-questions', {})
+  })
+
+  router.post('/' + version + '/baseline-checks/single-page-questions', function (req, res) {
+    res.redirect('../project-task-list')
+  })
+
+}
---
+++ app/routes/0-16/complete-processing-of-conversion-grant.js
@@ -0,0 +1,13 @@
+module.exports = function (router) {
+
+  var version = "0-16";
+
+  router.get('/' + version + '/complete-processing-of-conversion-grant/single-page-questions', function (req, res) {
+    res.render(version + '/complete-processing-of-conversion-grant/single-page-questions', {})
+  })
+
+  router.post('/' + version + '/complete-processing-of-conversion-grant/single-page-questions', function (req, res) {
+    res.redirect('../project-task-list')
+  })
+
+}

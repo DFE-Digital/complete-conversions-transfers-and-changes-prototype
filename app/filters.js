1b064bee79b2a006d9f06699afc81ac7a83a8a51
titlescreen <richardcooley@gmail.com>
2023-02-17 15:19:12 +0000

    Create rough monthly openers page
    Also adds in a month filter to out put this month + N

--- app/filters.js
+++ app/filters.js
@@ -1,2 +1,8 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const addFilter = govukPrototypeKit.views.addFilter
+
+addFilter('insertMonth', (input, numberOfMonthsAhead = 0) => {
+  const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+  const nextMonthName = monthNames[(new Date().getMonth() + numberOfMonthsAhead) % 12]
+  return input.replace('__month_name__', nextMonthName)
+})

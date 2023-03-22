ad7edaf526d082cf94ebf82b5c6b1e9fddf420a3
titlescreen <richardcooley@gmail.com>
2023-03-22 10:39:22 +0000

    Increment to v19

---
+++ app/routes/0-19/land-issues.js
@@ -0,0 +1,44 @@
+module.exports = function (router) {
+
+  var version = "0-19";
+
+  router.get('/' + version + '/land-issues/single-page-questions', function (req, res) {
+    res.render(version + '/land-issues/single-page-questions', {})
+  })
+
+  router.post('/' + version + '/land-issues/single-page-questions', function (req, res) {
+    const checkDescribeLandIssues = req.session.data['describeLandIssues']
+
+    if (checkDescribeLandIssues.includes('describeLandAndLeasingIssues') &
+    checkDescribeLandIssues.includes('describeSharedUsageIssues') ){
+      req.session.data.describeLandIssuesStatus = 'completed'
+    }else if (checkDescribeLandIssues == ('empty')){
+      req.session.data.describeLandIssuesStatus = 'notStarted'
+    }else if (checkDescribeLandIssues.includes('notApplicable')){
+      req.session.data.describeLandIssuesStatus = 'notApplicable'
+    }else {
+      req.session.data.describeLandIssuesStatus = 'inProgress'
+    }
+
+
+
+    res.redirect('../project-task-list')
+  })
+
+  router.get('/' + version + '/land-issues/land-building-leasing-arrangements-or-transfer-agreements', function (req, res) {
+    res.render(version + '/land-issues/land-building-leasing-arrangements-or-transfer-agreements', {})
+  })
+
+  router.post('/' + version + '/land-issues/land-building-leasing-arrangements-or-transfer-agreements', function (req, res) {
+    res.redirect('single-page-questions')
+  })
+
+  router.get('/' + version + '/land-issues/shared-usage-agreements', function (req, res) {
+    res.render(version + '/land-issues/shared-usage-agreements', {})
+  })
+
+  router.post('/' + version + '/land-issues/shared-usage-agreements', function (req, res) {
+    res.redirect('single-page-questions')
+  })
+
+}
---
+++ app/routes/0-19/land-registry-title-plans.js
@@ -0,0 +1,21 @@
+module.exports = function (router) {
+
+  var version = "0-19";
+
+  router.get('/' + version + '/land-registry-title-plans/single-page-questions', function (req, res) {
+    res.render(version + '/land-registry-title-plans/single-page-questions', {})
+  })
+
+  router.post('/' + version + '/land-registry-title-plans/single-page-questions', function (req, res) {
+    const clearLandRegistry = req.session.data['clearLandRegistry']
+
+    if (clearLandRegistry != 'Yes, the Land registry title plans or site plans have been cleared'){
+      req.session.data.landRegistryStatus = 'inProgress'
+    } else {
+      req.session.data.landRegistryStatus = 'completed'
+    }
+
+    res.redirect('../project-task-list')
+  })
+
+}
